c95fd9123f9c3a0fcb07d84c01f1c0003a3d1fe6
cgroane <connorgroane@utexas.edu>
2024-08-20 15:13:42 -0700

    remove extra slash

--- src/api/index.ts
+++ src/api/index.ts
@@ -8,7 +8,7 @@ const axiosInstance = axios.create({
   },
 });
 export const cfbdApi = axios.create({
-  baseURL: `${process.env.APP_ENV === 'development' ? 'http://localhost:3001/api/' : '/'}/api/`,
+  baseURL: `${process.env.APP_ENV === 'development' ? 'http://localhost:3001/api/' : ''}/api/`,
 });
 
 export const theOddsInstance = axios.create({
